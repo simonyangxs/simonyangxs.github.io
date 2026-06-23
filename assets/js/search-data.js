4ce806e9dbdfaf6c77f4d7cfe5d14fafbeb407bc
simonyangxs <32233157+simonyangxs@users.noreply.github.com>
2026-06-23 03:27:52 +0000

    Deploying to gh-pages from @ simonyangxs/simonyangxs.github.io@7a54f555db722fceb1787128aff42efa76a38dac 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "Blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
-        },{id: "nav-papers",
+  },{id: "nav-papers",
           title: "Papers",
           description: "",
           section: "Navigation",
